f772c45bcf165d0614de322ecec411a1ce4caac5
João Paulo Gomes <joao.div@gmail.com>
2021-03-15 20:49:39 -0300

    Criando as interfaces

---
+++ Interfaces/IData.cs
@@ -0,0 +1,15 @@
+﻿using DioSeries.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DioSeries.Interfaces
+{
+    public interface IData<T> where T : Entity
+    {
+        public Task<ICollection<T>> Get();
+        public Task Save(ICollection<T> entites);
+    }
+}
---
+++ Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using DioSeries.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DioSeries.Interfaces
+{
+    public interface IRepository<T> where T : Entity
+    {
+        List<T> List();
+        T Get(Guid id);
+        void Insert(T entity);
+        void Delete(Guid id);
+        void Update(Guid id, T entity);
+        bool Commit();
+    }
+}
