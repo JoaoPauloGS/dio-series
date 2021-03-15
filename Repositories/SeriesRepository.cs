86c74fb8f42dd3e57c173f2961014782d1462b7e
João Paulo Gomes <joao.div@gmail.com>
2021-03-15 20:49:56 -0300

    Criando repository

---
+++ Repositories/SeriesRepository.cs
@@ -0,0 +1,43 @@
+﻿using DioSeries.Entities;
+using DioSeries.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DioSeries.Repositories
+{
+    public class SeriesRepository : IRepository<Serie>
+    {
+        public bool Commit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Serie Get(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Serie entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Serie> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Guid id, Serie entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
