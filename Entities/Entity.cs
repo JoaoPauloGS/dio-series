e104b457ad5e30d44ae69f751fb8ef193dadaf2c
João Paulo Gomes <joao.div@gmail.com>
2021-03-15 20:17:20 -0300

    Criando Entidade base

---
+++ Entities/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DioSeries.Entities
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+        public bool Excluded { get; set; }
+    }
+}
